9e60446adf345c2212bfa2806604c867904cbe5e
Alyssa Colistro <arcolistro@gmail.com>
2020-02-14 08:13:36 -0800

    initial commit with webpack template.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Giphy API',
+      title: 'Week 6',
       template: './src/index.html',
       inject: 'body'
     })
