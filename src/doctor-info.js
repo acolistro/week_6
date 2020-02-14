e52e75acd926cbbf6e4d50efdd52a93da0eb6a19
Student <example@example.com>
2020-02-14 14:01:05 -0800

    Refactoring main.js and docinfo class.

--- src/doctor-info.js
+++ src/doctor-info.js
@@ -1,11 +1,12 @@
 export class DoctorInfo {
   constructor(doctor) {
     this.doctor = doctor;
-    this.name = name;
-    this.practice = practice;
-    this.address = address;
-    this.phone = phone;
-    this.acceptsNew = acceptsNew;
+    this.name = (doctor.profile.first_name + " " + doctor.profile.last_name).toUpperCase();
+    this.bio = "Bio: " + doctor.profile.bio;
+    this.practice = "Practices:" + doctor.practices[0].name;
+    this.address = "Address: " + doctor.practices[0].visit_address.street + ", " + doctor.practices[0].visit_address.city + ", " + doctor.practices[0].visit_address.state;
+    this.phone = doctor.practices[0].phones[0].number;
+    // this.website = website;
   }
 
   acceptingNew() {
--- src/main.js
+++ src/main.js
@@ -4,49 +4,52 @@ import './styles.css';
 import $ from 'jquery';
 
 import { Doctor } from './../src/doctor.js';
+import { DoctorInfo } from './../src/doctor-info.js';
 
 $(document).ready(function() {
 
-  $('#getDoctor').click(function() {
+  $('#getDoctorName').click(function() {
     const name = $('#name').val();
     $('#name').val("");
-    console.log(name);
     
-
     (async () => {
       let doctor = new Doctor();
       const response = await doctor.getDoctorByName(name);
+      console.log(response);
       getElements(response);
+      if (doctor.errorMessage) {
+        $(".errors").append(doctor.errorMessage);
+      }
     })();
 
-    async function getElements(response) {
-      const docArray = response;
-      let docNameArray = [];
-      
-      docArray.data.forEach(function(doctor) {
-        let newPatients = [];
-        console.log(newPatients);
-        docNameArray.push(doctor.profile.first_name + " " + doctor.profile.last_name);
-        acceptNew.push(doctor.practices);
-        console.log(doctor.profile.bio);
-        doctor.practices.forEach(function(practice) {
-          newPatients.push(practice.accepts_new_patients);
-          console.log(docArray.data.practices);
-        });
+    async function getElements(response) {      
+      response.data.forEach(function(doctor) {
+        let docInfo = new DoctorInfo(doctor);
+        docInfo.acceptingNew();
+        $(".show_by_name").append(
+          '<div class="panel panel-default">' + 
+            '<div class="panel-heading">' +
+              '<h2 class="panel-title">' + docInfo.name + '</h2>' + '</div>' + 
+              '<div class="panel-body">' + '<h2 id="doc-bio">' + docInfo.bio + '</h2>'
+        );
+       
+
       });
       
 
-      for (let i=0; i<docNameArray.length; i++) {
-        docNameArray[i].toUpperCase();
-        $('.panel-title').append(docNameArray[i]);
-        $('.showBio').append("<li>" + "Doctor Name: " + docNameArray[i] + "</li>");
-      if (newPatients === true) {
-        $('.acceptingNew').append("<li>" + "This doctor is accepting new patients! " + "</li>");
-      } else {
-        $('.acceptingNew').append("<li>" + "This doctor is NOT accepting new patients! " + "</li>");
-      }
-    }
-  }
+  //     for (let i=0; i<docArray.length; i++) {
+  //       docNameArray[i].toUpperCase();
+  //       $('.panel-title').append(docNameArray[i]);
+  //       $('.showBio').append("<li>" + "Doctor Name: " + docNameArray[i] + "</li>");
+  //     if (newPatients === true) {
+  //       $('.acceptingNew').append("<li>" + "This doctor is accepting new patients! " + "</li>");
+  //     } else {
+  //       $('.acceptingNew').append("<li>" + "This doctor is NOT accepting new patients! " + "</li>");
+  //     }
+  //   }
+  // }
 
-  });
+
+  }
+})
 });
